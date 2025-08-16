553d207492eab746dfb37460c792b22b5778994f
Andy Nguyen <iam.andynguwin@gmail.com>
2025-08-16 03:01:57 -0700

    React App + Custom Theme + Simple Routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <Routes>
+          <Route  path='/' element={<Dashboard/>}/>
+          <Route  path='/dashboard' element={<Dashboard/>}/>
+          <Route  path='/login' element={<Login/>}/>
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
