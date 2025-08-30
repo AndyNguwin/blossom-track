de808828decba64d64c54774c2dbffc9d8415f45
Andy Nguyen <iam.andynguwin@gmail.com>
2025-08-30 00:28:21 -0700

    Outline list of API endpoints to set up

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,25 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "First FastAPI app"}
+
+# Endpoints list
+
+## Authentication
+# auth/register
+# auth/login
+# auth/refresh
+# auth/signout
+
+## Tasks
+# tasks
+# tasks/{task_id}
+# POST task
+# PUT tasks/{task_id}
+# PATCH? tasks/task_id/archive
+# DELETE tasks/{task_id}
+
+## Completion
+# tasks/{task_id}/completions
+# POST /completions
+# DELETE completions/{task_id}/{date}
+# GET /dashboard?
